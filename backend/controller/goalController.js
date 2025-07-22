3c4d9981ab4e16f6c22a84a4d1f446a833fecfca
SethKkorir <kipzseth@gmail.com>
2025-07-22 16:52:28 +0300

    we are getting started

---
+++ backend/controller/goalController.js
@@ -0,0 +1,41 @@
+ const asyncHandler = require('express-async-handler');
+ // @desc Get Goals
+// @route GET /api/goals
+// @access Public
+// This is the goalController.js file that handles the logic for the goal routes.
+ const getGoals = asyncHandler (async(req, res) => {
+    if(!req.body.text) {
+        return res.status(400)
+        throw new Error('Please add a text field');
+    }
+  console.log(req.body);
+  res.status(200).json({ message: 'Get Goals' });
+});
+// @desc Set Goals
+const setGoals = asyncHandler(async(req, res) => {
+  console.log(req.body);
+  res.status(201).json({ message: 'Set Goals' });
+});
+// @desc Update Goal
+const updateGoal = asyncHandler(async(req, res) => {        
+    console.log(req.body);
+  console.log(`Update Goal with ID: ${req.params.id}`);
+    // Here you would typically update the goal in the database
+  res.status(200).json({ message: `Update Goal with ID: ${req.params.id}` });
+});
+
+const deleteGoal = asyncHandler(async(req, res) => {    
+    console.log(`Delete Goal with ID: ${req.params.id}`);
+  // Here you would typically delete the goal from the database
+  // For now, we will just return a success message
+  res.status(200).json({ message: `Delete Goal with ID: ${req.params.id}` });
+}); 
+// This is the goalController.js file that handles the logic for the goal routes.
+// It exports functions to get, set, update, and delete goals.
+// These functions will be used in the goalRoutes.js file to handle the respective HTTP requests.
+module.exports = {
+  getGoals,
+  setGoals,
+    updateGoal,
+    deleteGoal
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('helloworld')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const errorHandler = require('./middleware/errorMiddleware');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+// Error handling middleware
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
